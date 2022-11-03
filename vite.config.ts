59fa8071eb049e72c25c9ba56d06b703c4f9d72a
shimizu kenji <shimizu.kenji@noncore.net>
2022-11-03 18:51:07 +0900

    feat: viteの初期設定

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "./",
+  root: "./src",
+  publicDir: "../public",
+  build: {
+    outDir: "../dist",
+    emptyOutDir: true
+  },
+  server: {
+    open: true
+  },
   plugins: [react()]
 })
