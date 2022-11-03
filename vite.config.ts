8a7a3ad6995915a61c5b26060dda1ed45c408667
shimizu kenji <shimizu.kenji@noncore.net>
2022-11-03 22:03:16 +0900

    feat: router追加

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import pages from 'vite-plugin-pages'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,5 +14,8 @@ export default defineConfig({
   server: {
     open: true
   },
-  plugins: [react()]
+  plugins: [
+    react(),
+    pages({dirs: "./src/pages"})
+  ]
 })
