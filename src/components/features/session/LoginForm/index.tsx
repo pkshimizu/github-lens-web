eb7c6595728349fd0736c3e055236034135cdd0a
shimizu kenji <shimizu.kenji@noncore.net>
2022-11-09 23:36:57 +0900

    fix: エラーを解消

--- src/components/features/session/LoginForm/index.tsx
+++ src/components/features/session/LoginForm/index.tsx
@@ -1,6 +1,16 @@
 import LoginOAuth2 from '@okteto/react-oauth2-login'
+import { useCallback } from 'react'
 
 export default function LoginForm() {
+  const handleClick = useCallback(() => {
+    console.log('Click')
+  }, [])
+  const handleSuccess = useCallback((response: { code: string }) => {
+    console.log(response.code)
+  }, [])
+  const handleFailure = useCallback((response: { message: string }) => {
+    console.log(response.message)
+  }, [])
   return (
     <LoginOAuth2
       className={''}
@@ -8,9 +18,9 @@ export default function LoginForm() {
       scope={'code'}
       authorizeUri={'https://github.com/login/oauth/authorize'}
       redirectUri={'http://localhost:5173/oauth_callback'}
-      onClick={() => {}}
-      onSuccess={() => {}}
-      onFailure={() => {}}
+      onClick={handleClick}
+      onSuccess={handleSuccess}
+      onFailure={handleFailure}
     />
   )
 }
